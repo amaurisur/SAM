55da57a8454614e18b8cb8dee26466263bed7e54
amaurisur <mauricioguillermomorales@gmail.com>
2014-07-14 21:27:36 -0300

    Primera consulta (xquery) sobre LEP andando, en la version tipo acceso a Wiki con Java. Ver documentacion sobre indicadores en DRIVE

--- src/sam/WikiExample.java
+++ src/sam/WikiExample.java
@@ -1,4 +1,4 @@
-package sam1;
+package sam;
 
 
 import org.basex.core.*;
@@ -30,41 +30,44 @@ public final class WikiExample {
 
     // Use internal parser to skip DTD parsing
     new Set("intparse", true).execute(context);
-
-    final String doc = "http://en.wikipedia.org/wiki/Wikipedia";
+    //final String doc = "http://en.wikipedia.org/wiki/Wikipedia";
+    final String doc = "file:///C:/Users/sarli/Google%20Drive/Ingenieria/TPDisenoIng/WikiSEI/WikiSEI/wiki.sei.cmu.edu/sad/index.php/System_Overview.html";
     new CreateDB("WikiExample", doc).execute(context);
 
     // -------------------------------------------------------------------------
     // Insert a node before the closing body tag
     // N.B. do not forget to specify the namespace
-    System.out.println("\n* Update the document.");
-
-    new XQuery(
-        "declare namespace xhtml='http://www.w3.org/1999/xhtml';" +
-        "insert node " +
-        "  <p>I will match the following query because I contain " +
-        "the terms 'ARTICLE' and 'EDITABLE'. :-)</p> " +
-        "into //body"
-    ).execute(context);
-
+//    System.out.println("\n* Update the document.");
+//
+//    new XQuery(
+//        "declare namespace xhtml='http://www.w3.org/1999/xhtml';" +
+//        "insert node " +
+//        "  <p>I will match the following query because I contain " +
+//        "the terms 'ARTICLE' and 'EDITABLE'. :-)</p> " +
+//        "into //body"
+//    ).execute(context);
+//
     // ----------------------------------------------------------------------
     // Match all paragraphs' textual contents against
     // 'edit.*' AND ('article' or 'page')
     System.out.println("\n* Perform a full-text query:");
-
-    System.out.println(new XQuery(
-        "declare namespace xhtml='http://www.w3.org/1999/xhtml';" +
+    
+/*
+    "declare namespace xhtml='http://www.w3.org/1999/xhtml';" +
         "for $x in //p/text()" +
         "where $x contains text ('edit.*' ftand ('article' ftor 'page')) " +
         "  using wildcards distance at most 10 words " +
         "return <p>{ $x }</p>"
+*/
+    System.out.println(new XQuery("declare namespace xhtml='http://www.w3.org/1999/xhtml';" +
+            "//*[@id='bodyContent']/descendant-or-self::*[text() contains text 'QAS1' using wildcards]"        
     ).execute(context));
-
+  
     // ----------------------------------------------------------------------
     // Drop the database
     System.out.println("\n* Drop the database.");
 
-    new DropDB("WikiExample").execute(context);
+    //new DropDB("WikiExample").execute(context);
 
     // ------------------------------------------------------------------------
     // Close the database context
