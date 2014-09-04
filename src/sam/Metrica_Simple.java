e565003c1fcd4ed18c9f90cd681cd3c9d7e2aca7
amaurisur <mauricioguillermomorales@gmail.com>
2014-09-04 18:26:02 -0300

    cambios

---
+++ src/sam/Metrica_Simple.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sam;
+
+/**
+ *
+ * @author sarli
+ */
+public class Metrica_Simple extends Indicador {
+
+    public Metrica_Simple(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void agregar(Indicador i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar(Indicador i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void evaluar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
